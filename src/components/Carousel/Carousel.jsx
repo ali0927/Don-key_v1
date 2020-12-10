04de9ddb762ccafbfbc82c973affcfeda12e4752
ali0927 <kevinali0927@gmail.com>
2020-12-10 19:00:38 +0530

    Add customize Back & Next button to Slider

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
 import "./CarouselStyle.scss";
 
 export default function ControlledCarousel(props) {
@@ -10,7 +10,13 @@ export default function ControlledCarousel(props) {
   };
 
   return (
-    <Carousel prevLabel="previous" activeIndex={index} onSelect={handleSelect}>
+  <Carousel
+    indicators={false}
+    prevLabel="Back"
+    nextLabel="Next"
+    activeIndex={index}
+    onSelect={handleSelect}
+  >
       <Carousel.Item>{props.children1}</Carousel.Item>
       <Carousel.Item>{props.children2}</Carousel.Item>
       <Carousel.Item>{props.children3}</Carousel.Item>
