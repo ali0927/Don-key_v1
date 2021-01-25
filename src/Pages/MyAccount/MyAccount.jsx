76df2f23ef9f4aa1c9cf827be6e3f1f1579dc323
ali0927 <kevinali0927@gmail.com>
2021-01-25 21:50:37 +0200

    Loader

--- src/Pages/MyAccount/MyAccount.jsx
+++ src/Pages/MyAccount/MyAccount.jsx
@@ -20,7 +20,7 @@ import ButtonComponent from "../../components/Button/Button";
 import DummyData from "../../JsonData/MyAccountDetail";
 import DataFarmer from "../../JsonData/DataFarmer";
 import StrategiesTable from "../../JsonData/StrategiesTable";
-import animation from "../../images/animation.gif";
+import animation from "../../images/animation_500_kkczfqhd.gif";
 import { waitFor } from "../../helpers/helpers";
 
 const MyAccount = () => {
@@ -54,7 +54,7 @@ const MyAccount = () => {
       {!isReady &&
         createPortal(
           <div className="loading">
-            <img src={animation} alt="Animation" />
+            <img src={animation} alt="Animation" width="500" />
           </div>,
           document.body
         )}
