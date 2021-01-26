32dc1a7d91b738418da9b755bcca94676e4ea716
ali0927 <kevinali0927@gmail.com>
2021-01-26 16:12:34 +0530

    Added Api Endpoint For Map

--- backend/src/controllers/ProtocolsController.ts
+++ backend/src/controllers/ProtocolsController.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from "express";
 import { prisma } from "../database";
-import { protocol_proto_map } from "@prisma/client";
 import sharp from "sharp";
 import axios from "axios";
+import { sendResponse } from "../helpers/sendResponse";
 const Protocols = prisma.protocols;
-
+const ProtocolMap = prisma.protocol_proto_map;
 const convertoDataUri = async (url: string) => {
     const input = (await axios({ url, responseType: "arraybuffer" }))
         .data as Buffer;
@@ -14,26 +14,36 @@ const convertoDataUri = async (url: string) => {
     return "data:image/png;base64," + uri.toString("base64");
 }
 
-const replaceWithDateUri = async (item: protocol_proto_map) => {
-
-    item.image = await convertoDataUri(item.image)
-};
 
 export class ProtocolsController {
-    static getProtocols: RequestHandler = async (req, res) => {
+
+    static getMapImage: RequestHandler = async (req, res) => {
+        let {protocol_id, nextprotocol_id} = req.query;
+      
+        const results = await ProtocolMap.findFirst({where: {protocol_id:  parseInt(protocol_id as string),
+            next_protocol_id: parseInt(nextprotocol_id as string)}})
+        if(!results){
+            return sendResponse(res,{code: 404, error: {msg: "Not Found"}});
+        }
+        sendResponse(res, {data: results.image})
+    }
+
+    static getProtocols: RequestHandler = async (req, res,next) => {
+        if(req.query.protocol_id && req.query.nextprotocol_id){
+            return ProtocolsController.getMapImage(req,res, next);
+        }
+
         const results = await Protocols.findMany({
-            include: { actions: true, protocolMaps: true },
+            include: { actions: true,  },
         });
 
         const newResults: (typeof results[1] & { base64: string })[] = [];
-        const promises: Promise<any>[] = [];
-        const base64Urls = results.map(({ vertexImageURL, protocolMaps }) => {
-            protocolMaps.forEach((item) => {
-                promises.push(replaceWithDateUri(item));
-            });
+ 
+        const base64Urls = results.map(({ vertexImageURL }) => {
+     
             return convertoDataUri(vertexImageURL as string);
         });
-        const uris = await Promise.all([...base64Urls, ...promises]);
+        const uris = await Promise.all(base64Urls);
 
         for (let i = 0; i < results.length; i++) {
             const result = results[i];
