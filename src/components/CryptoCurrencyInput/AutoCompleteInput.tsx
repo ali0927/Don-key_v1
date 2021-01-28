421f7b823f08a8cdecc383c4d066f03a905be3fd
ali0927 <kevinali0927@gmail.com>
2021-01-28 16:09:01 +0530

    Added Virtualized List

--- src/components/CryptoCurrencyInput/AutoCompleteInput.tsx
+++ src/components/CryptoCurrencyInput/AutoCompleteInput.tsx
@@ -3,8 +3,7 @@ import React, { useCallback, useMemo, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import Fuse from "fuse.js";
 import { ICurrency } from "interfaces";
-
-
+import { List, AutoSizer } from "react-virtualized";
 
 export const AutoCompleteInput = <T extends ICurrency>({
 	currencies,
@@ -27,7 +26,7 @@ export const AutoCompleteInput = <T extends ICurrency>({
 		}
 
 		return fuse.search(inputValue).map((val) => val.item);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [inputValue]);
 
 	const onInputChange = useCallback((e) => {
@@ -42,6 +41,52 @@ export const AutoCompleteInput = <T extends ICurrency>({
 	};
 
 	const handleKeyDown = () => { };
+
+	const renderRow = ({
+		index,
+		style
+	}: {
+		index: number;
+		style: any
+	}) => {
+		const currency = filteredCurrencies[index];
+		const getIcon = () => {
+			if (multi) {
+				return (
+					<>
+						<img src={currency.tokenIcon} />{" "}
+						<img src={currencies[getNextIndex(index)].tokenIcon} />
+					</>
+				);
+			}
+			return <img src={currency.tokenIcon} />;
+		};
+		const getName = () => {
+			if (multi) {
+				return (
+					<>
+						{currency.tokenSymbol}/{currencies[getNextIndex(index)].tokenSymbol}
+					</>
+				);
+			}
+			return currency.tokenSymbol;
+		};
+		return (
+			<div
+				key={currency.tokenSymbol}
+				onClick={() => onSelect(currency)}
+				className="cryptoinput__list__item"
+				style={style}
+			>
+				<span className="cryptoinput__icon">{getIcon()}</span>
+				<span className="cryptoinput__name">{getName()}</span>
+				<span className="cryptoinput__percent">
+					{currency.apy_apyOneMonthSample.toFixed(2)}%<span>APY</span>
+				</span>
+				<span className="cryptoinput__value">{currency.balance}</span>
+			</div>
+		);
+	};
 
 	return (
 		<div className="cryptoinput__wrapper">
@@ -59,42 +104,18 @@ export const AutoCompleteInput = <T extends ICurrency>({
 				type="text"
 			/>
 			<div className="cryptoinput__list">
-				{filteredCurrencies.map((currency, index) => {
-					const getIcon = () => {
-						if (multi) {
-							return (
-								<>
-									<img src={currency.tokenIcon}/> <img src={currencies[getNextIndex(index)].tokenIcon} />
-								</>
-							);
-						}
-						return <img src={currency.tokenIcon} />;
-					};
-					const getName = () => {
-						if (multi) {
-							return (
-								<>
-									{currency.tokenSymbol}/{currencies[getNextIndex(index)].tokenSymbol}
-								</>
-							);
-						}
-						return currency.tokenSymbol;
-					};
-					return (
-						<div
-							key={currency.tokenSymbol}
-							onClick={() => onSelect(currency)}
-							className="cryptoinput__list__item"
-						>
-							<span className="cryptoinput__icon">{getIcon()}</span>
-							<span className="cryptoinput__name">{getName()}</span>
-							<span className="cryptoinput__percent">
-								{currency.apy_apyOneMonthSample.toFixed(2)}%<span>APY</span>
-							</span>
-							<span className="cryptoinput__value">{currency.balance }</span>
-						</div>
-					);
-				})}
+				<AutoSizer>
+					{({ height, width }) => {
+						return <List
+							height={height}
+							width={width}
+							rowHeight={50}
+							rowRenderer={renderRow}
+							rowCount={filteredCurrencies.length}
+						/>
+					}}
+				</AutoSizer>
+
 			</div>
 		</div>
 	);
