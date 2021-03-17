f9928c4b10942de6b9dc219a0becab2bfa771356
ali0927 <kevinali0927@gmail.com>
2021-03-17 10:10:02 +0530

    Fixed Admin Bug

--- byadmin/src/hooks/useInputState.tsx
+++ byadmin/src/hooks/useInputState.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { useCallback } from "react";
 import { tuplify } from "../helpers/tuplify";
+import { isObject} from "lodash";
 
 export const useInputState = (val?: string) => {
   const [state, setState] = useState(val);
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if(!("target" in e)){
+    if(!isObject(e)){
       setState(e);
     }else {
       setState(e.target.value);
