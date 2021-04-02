b504228ccf022a570625314529d4fb710b5d05d0
ali0927 <kevinali0927@gmail.com>
2021-04-02 20:50:10 +0530

    Added TeamPage

--- src/Pages/TeamPage/TeamPage.tsx
+++ src/Pages/TeamPage/TeamPage.tsx
@@ -1,17 +1,110 @@
 import { NavBar2 } from "../../components/Navbar/NavBar";
 import { Container, Form } from "react-bootstrap";
 import { Footer } from "components/Footer/Footer";
-import ButtonComponent from "components/Button/Button";
+import gil from "./gil.jpeg";
+import yon from "./yonatan.jpeg";
+import alon from "./alon.jpeg";
+import yanir from "./yanir.jpeg";
+import daniel from "./daniell.jpeg";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
+
+export type ITeamCardProps = {
+  name: string;
+  designation: string;
+  twitterlink?: string;
+  linkedLink?: string;
+  image: string;
+};
+
+const TeamCard = ({
+  name,
+  designation,
+  image,
+  twitterlink,
+  linkedLink,
+}: ITeamCardProps) => {
+  return (
+    <div className="col-md-3 mb-5">
+      <div
+        className="shadow-sm py-5 p-3 d-flex flex-column justify-content-between align-items-center h-100"
+        style={{ background: "#fff", borderRadius: 4 }}
+      >
+        <div
+          style={{
+            borderRadius: "50%",
+            overflow: "hidden",
+            width: 100,
+            height: 100,
+          }}
+        >
+          <img className="team-image img-fluid" src={image} />
+        </div>
+        <h3 className="mt-4 text-center">{name}</h3>
+        <p>{designation}</p>
+
+        {twitterlink && (
+          <a href={twitterlink}>
+            <FaTwitter />
+          </a>
+        )}
+        {linkedLink && (
+          <a href={linkedLink}>
+            <FaLinkedin />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const TeamJSON: ITeamCardProps[] = [
+  {
+    image: gil,
+    name: "Gil Shpirman",
+    designation: "Co-founder",
+    twitterlink: "https://twitter.com/shpirman",
+  },
+  {
+    image: yon,
+    name: "Yonatan Martsiano",
+    designation: "Co-founder & Developer",
+    linkedLink: "https://www.linkedin.com/in/yonatan-martsiano-6376a314a/",
+  },
+  {
+    image: alon,
+    name: "Alon Hershberg",
+    designation: "Advisor",
+    linkedLink: "https://www.linkedin.com/in/alon-herschberg-38876a13a/",
+  },
+  {
+    image: yanir,
+    name: "Yanir",
+    designation: "Co-founder",
+    linkedLink: "https://www.linkedin.com/in/yanir-goldenberg-61074887/",
+  },
+  {
+    image: daniel,
+    name: "Daniel Jaffa",
+    designation: "Co-founder & Developer",
+    linkedLink: "https://www.linkedin.com/in/daniel-j-23016211",
+  },
+];
+
 const TeamPage = () => {
   return (
     <div className="bgnav">
       <NavBar2 hideWallet />
       <div className=" pt-5 pb-5">
         <Container>
-          <div className="row">
+          <div className="row ">
             <div className="col">
               <h1 className="text-center mt-md-3">Our Team</h1>
             </div>
+          </div>
+          <div className="row mt-5 justify-content-center">
+            {TeamJSON.map((team) => {
+              return <TeamCard key={team.name} {...team} />;
+            })}
           </div>
         </Container>
       </div>
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -25,17 +25,25 @@ const useWalletAddress = ({ short = false }) => {
 const Logo = () => {
   return (
     <Navbar.Brand onClick={(e) => e.preventDefault()}>
-      <Link className="d-flex align-items-center inherit-color no-underline" to="/">
-        <img
-          src="/assets/images/logo.png"
-          className="d-inline-block align-top by-logo"
-          alt="Logo"
-        />
+      <Link
+        className="d-flex align-items-center inherit-color no-underline"
+        to="/"
+      >
         <p
           className="m-0"
-          style={{ fontFamily: "Avenir-Regular", fontWeight: "bold" }}
+          style={{ fontFamily: "Avenir-Bold", fontWeight: "bolder" }}
         >
-          <span style={{ letterSpacing: "2px" }}>Don</span>-key
+          <span style={{ letterSpacing: "2px" }}>
+            D
+            <img
+              style={{ position: "relative", width: 24,top: 2, marginLeft: -2, marginRight:-1 }}
+              src="/assets/images/logo.png"
+              className="d-inline-block align-top by-logo"
+              alt="Logo"
+            />
+            n
+          </span>
+          {" "}-{" "}key
         </p>
       </Link>
     </Navbar.Brand>
