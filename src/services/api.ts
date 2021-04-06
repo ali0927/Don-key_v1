bdd3e0e81251de56b16a49e045ff1a9d5bf0b49a
ali0927 <kevinali0927@gmail.com>
2021-04-06 22:07:50 +0530

    Changed Api

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { getWeb3 } from "helpers/helpers";
 
 
 export const api = axios.create({
-  baseURL: "https://api.by.finance",
+  baseURL: "https://api.don-key.finance",
   transformRequest: [...axios.defaults.transformRequest as any, (data,headers) => {
     const token =localStorage.getItem(AuthToken)
     if(token){
