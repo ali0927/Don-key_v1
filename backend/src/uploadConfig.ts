ca9b62d63799a8502d5a59ea5e644431f7bbde0b
ali0927 <kevinali0927@gmail.com>
2021-04-07 15:26:11 +0530

    Updated upload config

--- backend/src/uploadConfig.ts
+++ backend/src/uploadConfig.ts
@@ -1,4 +1,12 @@
 import multer from "multer"
 import path from "path";
+import {uuidv4} from "./helpers"
+const storage = multer.diskStorage({
+    destination: path.resolve(__dirname, "uploads") ,
+    filename: function (req, file, cb) {
+        cb(null, req.user?.walletAddress || uuidv4() + ".png")
+      }
+})
 
-export const upload = multer({ dest: path.resolve(__dirname, "uploads") });
+
+export const upload = multer({ storage });
