692f1c66aec4858025347bd5596e3a4e1b1d6185
ali0927 <kevinali0927@gmail.com>
2021-04-09 09:45:06 +0530

    Updated Animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,21 +1,23 @@
 import clsx from "clsx";
 import React from "react";
-import "./Loader.scss"
+import Lottie from "react-lottie";
+import animationData from "./loader.json";
 
-export const Loader = ({className, style}: {className?: string; style?: React.CSSProperties }) => {
-    return <div className={clsx("preloader_main", className)} style={style}>
-    <div className="preloader_main_inner">
-        <div className="round_1"></div>
-        <div className="round-rotate">
-            <div className="round-group">
-                <div className="round_2 round-common"></div>
-                <div className="round_3 round-common"></div>
-            </div>
-            <div className="round-group">
-                <div className="round_4 round-common"></div>
-                <div className="round_5 round-common"></div>
-            </div>
-        </div>
-    </div>
-</div>
-}
+export const Loader = ({ style = {} }: { style?: React.CSSProperties }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <Lottie
+      width={400}
+      style={{ maxWidth: "100%", pointerEvents: "none", ...style }}
+      isClickToPauseDisabled
+      options={defaultOptions}
+    />
+  );
+};
