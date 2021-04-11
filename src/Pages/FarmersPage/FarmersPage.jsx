e650be84e4b598b32fe2dc666277ec4693c25291
ali0927 <kevinali0927@gmail.com>
2021-04-11 10:35:36 +0530

    Fixed Netlify Form (#45)
    * Fixed Netlify Form
    
    * Fixed Form
    
    * Fixed Farmers Page
    
    * Fix
    
    * Changes
    
    * Fixed Form

--- src/Pages/FarmersPage/FarmersPage.jsx
+++ src/Pages/FarmersPage/FarmersPage.jsx
@@ -3,6 +3,8 @@ import { Container, Form } from "react-bootstrap";
 import "./FarmersStyle.scss";
 import { Footer } from "components/Footer/Footer";
 import ButtonComponent from "components/Button/Button";
+import { useState } from "react";
+import axios from "axios";
 const FarmersPage = () => {
   return (
     <div className="bgnav">
@@ -15,12 +17,15 @@ const FarmersPage = () => {
 
               <form
                 className="newStrategyContent"
+                name="newfarmer"
+                method="POST"
                 style={{ background: "#fff" }}
               >
                 <p className="text-center">
                   You think you are a super star farmer? come and show everyone!
                   Submit to become an early farmer
                 </p>
+                <input type="hidden" name="form-name" value="newfarmer" />
 
                 <div className="container">
                   <div className="row justify-content-center">
@@ -28,24 +33,31 @@ const FarmersPage = () => {
                       <p className="text-center font-weight-bold">
                         Please Enter Your details Below
                       </p>
+                      <p className="d-none">
+                        <input type="text" name="bot-field" />
+                      </p>
                       <Form.Group controlId="nickname">
                         <Form.Label>Nick name</Form.Label>
-                        <Form.Control placeholder="Nick name" />
+                        <Form.Control name="nickname" placeholder="Nick name" />
                       </Form.Group>
                       <Form.Group controlId="telegram">
                         <Form.Label>Telegram</Form.Label>
-                        <Form.Control placeholder="Telegram User" />
+                        <Form.Control
+                          name="telegram"
+                          placeholder="Telegram User"
+                        />
                       </Form.Group>
 
                       <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>What kind of Don-key are you?</Form.Label>
                         <Form.Control
                           as="textarea"
+                          name="description"
                           placeholder="I'm the kind of farmer you all want to farm"
                           rows={3}
                         />
                       </Form.Group>
-                      <ButtonComponent className="btnYellow">
+                      <ButtonComponent type="submit" className="btnYellow">
                         Submit
                       </ButtonComponent>
                     </div>
@@ -57,146 +69,6 @@ const FarmersPage = () => {
         </Container>
       </div>
 
-      {/* Table */}
-      {/* <div className="mt-4 mb-5">
-        <Container>
-          <div className="tablebg">
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>RANK</th>
-                  <th></th>
-                  <th>NAME</th>
-                  <th>
-                    Budget
-                    <img
-                      src="/assets/images/icon1.png"
-                      className="d-inline-block pl-2"
-                      alt="Image"
-                    />
-                  </th>
-                  <th>AGE</th>
-                  <th> BURU TOKEN</th>
-                  <th>Chart</th>
-                  <th>APY</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {DataFarmer.map((item, index) => {
-                  var str = item.name;
-                  var res = str.substring(0, 2).toLocaleUpperCase();
-                  var number = item.apy;
-                  if (number.toString().length >= 3) {
-                    var num = number
-                      .toString()
-                      .replace(/\B(?=(\d{2})+(?!\d))/g, ",");
-                  } else {
-                    var num = number
-                      .toString()
-                      .replace(/\B(?=(\d{1})+(?!\d))/g, ",");
-                  }
-
-                  return (
-                    <>
-                      <tr>
-                        <td className="nodata">{item.id}</td>
-                        <td>
-                          <img
-                            src="/assets/images/blackstar.png"
-                            className="d-inline-block"
-                            alt="Image"
-                          />
-                        </td>
-                        <td>
-                          <span className="spanname"> {res}</span>
-                        </td>
-                        <td>
-                          <span className="spanbold">{item.name} </span> <br />
-                          <span className="fontlight">
-                            Nb of farmers: {item.nb_farmers}
-                          </span>
-                        </td>
-                        <td> ${item.budget}</td>
-                        <td>{item.age} years</td>
-                        <td className="fontlighBold">{item.buru_token} </td>
-                        <td className="tdGraphic">
-                          {item.chart_url ? (
-                            <img
-                              src={item.chart_url}
-                              className="d-inline-block"
-                              alt="Image"
-                            />
-                          ) : (
-                            <img
-                              src="/assets/images/graphic2.png"
-                              className="d-inline-block"
-                              alt="Image"
-                            />
-                          )}
-                        </td>
-                        <td className="fontdBold">{"+" + `${num}` + "%"}</td>
-                      </tr>
-                      {item.id == 2 ? (
-                        <tr>
-                          <td className="rowData" colspan="8">
-                            Join our farmers team
-                            <ButtonComponent
-                              variant="colorBlack"
-                              className="btnYellow ml-md-5 btnPadding"
-                            >
-                              <span> Join us </span>
-                            </ButtonComponent>
-                          </td>
-                          <td></td>
-                        </tr>
-                      ) : null}
-                    </>
-                  );
-                })}
-              </tbody>
-            </Table>
-
-            <div className="mt-4 pagePosition">
-              <p className="pageTable">Showing 1-10 of 120</p>
-              <div className="paginationTable">
-                <Pagination>
-                  <Pagination.Prev />
-                  <Pagination.Item active>{1}</Pagination.Item>
-                  <Pagination.Item>{2}</Pagination.Item>
-                  <Pagination.Item>{3}</Pagination.Item>
-                  <Pagination.Item>{4}</Pagination.Item>
-                  <Pagination.Ellipsis />
-
-                  <Pagination.Item>{120}</Pagination.Item>
-
-                  <Pagination.Next />
-                </Pagination>
-              </div>
-              <div className="dropTable">
-                Show rows
-                <span>
-                  <img
-                    src="/assets/images/selectdrop.png"
-                    className="d-inline-block align-top mr-3 ml-2 ml-md-0 mr-md-4"
-                    alt="Logo"
-                  />
-                </span>
-                <Form.Group>
-                  <Form.Control as="select">
-                    <option>100</option>
-                    <option>200</option>
-                    <option>300</option>
-                  </Form.Control>
-                </Form.Group>
-              </div>
-            </div>
-          </div>
-        </Container>
-      </div>
-
-      {/*  footer */}
       <Footer />
     </div>
   );
