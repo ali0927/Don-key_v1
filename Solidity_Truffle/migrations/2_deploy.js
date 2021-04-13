78aad88589672e5b76799c77997eaec9d15fac74
ali0927 <kevinali0927@gmail.com>
2021-04-13 20:22:55 +0700

    Smart contracts moved to its own repository 'Don-Key.finance-SmartContracts'

--- Solidity_Truffle/migrations/2_deploy.js
+++
@@ -1,6 +0,0 @@
-// migrations/2_deploy.js
-// SPDX-License-Identifier: MIT
-const bdollar = artifacts.require("BdollarInterface");
-module.exports = function(deployer) {
-  deployer.deploy(bdollar);
-};
