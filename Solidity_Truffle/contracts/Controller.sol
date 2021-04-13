44825b8445d94f640642f2a6217f9e124fe5bbd3
ali0927 <kevinali0927@gmail.com>
2021-04-13 09:17:42 +0530

    Update Controller.sol
    modified contract according to solidity coding standard

--- Solidity_Truffle/contracts/Controller.sol
+++ Solidity_Truffle/contracts/Controller.sol
@@ -1,37 +1,49 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.9;
 import "./IBEP20.sol";
+
 abstract contract Controller {
+
   mapping (address => bool) public admins;
+
+  receive() payable external {}
 
   constructor() public{
     admins[msg.sender]=true;
 
   }
-modifier onlyAdmin {    
+
+  modifier onlyAdmin {    
         require(admins[msg.sender]==true, "Only admin can do that");
         _;
     }
 
-
-  function getTOKEN(address token) public view returns (uint){
-
+  function getTOKEN(
+    address token
+  ) 
+    public view 
+    returns (uint256)
+  {
     IBEP20 TOKENt = IBEP20(token);
     return TOKENt.balanceOf(address(this));
-
   }
-  // important to receive ETH
-  receive() payable external {}
 
+  function addAdmin(
+    address admin
+  ) 
+    public 
+    onlyAdmin
+  {
+    admins[admin]=true;
+  }
 
-function addAdmin(address admin) public onlyAdmin{
-admins[admin]=true;
+  function checkadmin(
+    address check
+  ) 
+    public view 
+    returns (bool) 
+  {
+    return admins[check];
+  }
 
-}
-
-function checkadmin(address check) public view returns (bool) {
-return admins[check];
-
-}
-  // important to receive ETH
 }
