13e3f4026def748d0cfcfe8ea05d0a77ce88cf30
ali0927 <kevinali0927@gmail.com>
2021-04-14 15:34:30 +0530

    Adding Sentry (#73)

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,26 @@ import "./index.scss";
 import "./fonts/object-sans/ObjectSans-Bold.otf";
 import "./fonts/CodecPro/CodecPro-News.ttf";
 import { store } from "./store";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
+Sentry.init({
+  dsn:
+    "https://a02e8e703bab44d39b5cb4323ebce3ed@o571223.ingest.sentry.io/5719005",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.4,
+});
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
 );
