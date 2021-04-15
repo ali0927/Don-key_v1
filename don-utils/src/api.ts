56b371671ff3ae79d8b1d3c0d72f3af45af7a1a2
ali0927 <kevinali0927@gmail.com>
2021-04-15 15:28:02 +0530

    Fixed Builder API

--- don-utils/src/api.ts
+++ don-utils/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const AuthToken = "DonAuthToken";
 export const api = axios.create({
-    baseURL:  "https://api.dev.don-key.finance",
+    baseURL:  "https://api.don-key.finance",
     transformRequest: [...axios.defaults.transformRequest as any, (data,headers) => {
       const token = localStorage.getItem(AuthToken)
       if(token){
