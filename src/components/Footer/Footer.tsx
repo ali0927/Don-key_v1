401eb3367f33022ac2f256aa5f274b6e9e3ecab9
ali0927 <kevinali0927@gmail.com>
2021-05-15 14:48:09 +0300

    style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,36 +12,40 @@ export const Footer = () => {
     <CustomFooter className="pt-5 pb-5 mb-0">
       <Container>
         <Row>
-          <Col>
-            <div className="d-flex justify-content-between align-items-center">
-              <LogoWhite />
-              <div>
-                <p style={{ color: "#fff" }}>
-                  ERC-20 Contract address:
-                  <a
-                    href="https://etherscan.io/address/0x217ddead61a42369a266f1fb754eb5d3ebadc88a"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    0x217ddead61a42369a266f1fb754eb5d3ebadc88a
-                  </a>
-                </p>
-                <p style={{ color: "#fff" }}>
-                  BEP-20 Contract address:
-                  <a
-                    href="https://bscscan.com/address/0x86b3f23b6e90f5bbfac59b5b2661134ef8ffd255"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    0x86b3f23b6e90f5bbfac59b5b2661134ef8ffd255
-                  </a>
-                </p>
-              </div>
-              <p className="text-white ml-5"> All Rights Reserved 2021</p>
+          <Col sm="2">
+            <LogoWhite />
+          </Col>
+          {/* <div className="d-flex justify-content-between align-items-center"> */}
+          <Col sm="8" style={{ textAlign: "center" }}>
+            <div>
+              <p style={{ color: "#fff" }}>
+                ERC-20 Contract address:
+                <a
+                  href="https://etherscan.io/address/0x217ddead61a42369a266f1fb754eb5d3ebadc88a"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  0x217ddead61a42369a266f1fb754eb5d3ebadc88a
+                </a>
+              </p>
+              <p style={{ color: "#fff" }}>
+                BEP-20 Contract address:
+                <a
+                  href="https://bscscan.com/address/0x86b3f23b6e90f5bbfac59b5b2661134ef8ffd255"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  0x86b3f23b6e90f5bbfac59b5b2661134ef8ffd255
+                </a>
+              </p>
             </div>
           </Col>
+          <Col sm="2" style={{ textAlign: "center" }}>
+            <p className="text-white"> All Rights Reserved 2021</p>
+          </Col>
+          {/* </div> */}
         </Row>
       </Container>
     </CustomFooter>
