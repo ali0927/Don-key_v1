5c9e3c971b6a9e0993e18a49a33c1d02f51d7afd
ali0927 <kevinali0927@gmail.com>
2021-05-26 15:05:57 +0530

    Wallet Change Listener Updated

--- src/components/AccountChangeListener/AccountChangeListener.tsx
+++ src/components/AccountChangeListener/AccountChangeListener.tsx
@@ -1,25 +1,21 @@
-import { useMetaMaskLogin } from "hooks/useMetaMaskLogin";
+import { AuthToken } from "don-utils";
 import { useEffect } from "react";
 
-
-
 export const AccountChangeListener = () => {
-  const {doMetaMaskLogin} = useMetaMaskLogin()
   useEffect(() => {
     //@ts-ignore
-    
+
     if (typeof window !== "undefined" && window.ethereum) {
       const handleLogin = async (accounts: any) => {
         // Time to reload your interface with accounts[0]!
-        await doMetaMaskLogin();
-      }
-      //@ts-ignore
-      window.ethereum.on("accountsChanged",handleLogin);
+        localStorage.removeItem(AuthToken);
+        window.location.reload();
+      };
+      // @ts-ignore
+      window.ethereum.on("accountsChanged", handleLogin);
     }
-     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
   return null;
-
-}
+};
