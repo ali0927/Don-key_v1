74dd5e04c73d7462abb55c5730f7c8313f51c172
ali0927 <kevinali0927@gmail.com>
2021-06-09 18:44:58 +0530

    Fixed Env Logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import Routes from "./routes/Routes";
 import "./scss/styles.scss";
-import LogRocket from 'logrocket';
+import LogRocket from "logrocket";
 import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    if(process.env.NODE_ENV === "production"){
-      LogRocket.init('uisfg9/donkey');
-      console.error = () => {
-        
-      }
+    if (process.env.REACT_APP_ENV === "production") {
+      LogRocket.init("uisfg9/donkey");
+      console.error = () => {};
     }
-  }, [])
+  }, []);
   return <Routes />;
 }
 
