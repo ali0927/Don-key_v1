ba1d309893353759c5b2d68a39ce5aff2967345a
ali0927 <kevinali0927@gmail.com>
2021-07-08 14:21:38 +0530

    Faq Page Changes

--- src/Pages/Faq/Faq.tsx
+++ src/Pages/Faq/Faq.tsx
@@ -3,14 +3,9 @@ import { NavBar } from "../../components/Navbar/NavBar";
 import styled from "styled-components";
 import { theme } from "theme";
 import { Footer } from "components/Footer";
-import {
-    LargeEllipse,
-    MeadiumEllipse,
-    DonKeyIcon
-} from "icons";
+import { LargeEllipse, MeadiumEllipse, DonKeyIcon } from "icons";
 
 const Header = styled.div`
- 
   width: 100%;
   min-height: 400px;
   background: ${theme.palette.background.yellow};
@@ -22,27 +17,25 @@ const HeaderInner = styled.div`
   width: 70%;
 `;
 
-
-
 const HeadingTitle = styled.div`
-    flex: 1;
-    font-family: Roboto;
-    font-size: 40px;
-    font-style: normal;
-    font-weight: 800;
-    text-align: center;
-    color: ${theme.palette.text.black};
+  flex: 1;
+  font-family: Roboto;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 800;
+  text-align: center;
+  color: ${theme.palette.text.black};
 `;
 
 const HeaderContent = styled.p`
- font-family: Roboto;
- font-style: normal;
- font-weight: normal;
- font-size: 20px;
- line-height: 132%;
- display: flex;
- text-align: center;
- color: ${theme.palette.text.black};
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+ 
+  display: flex;
+  text-align: center;
+  color: ${theme.palette.text.black};
 `;
 
 const Body = styled.div`
@@ -54,41 +47,31 @@ const FrequentRoot = styled.div`
   background: ${theme.palette.common.white};
   width: 70%;
   border-radius: 10px;
-  margin-top: -50px;
+  margin-top: -130px;
   box-shadow: rgb(0 18 80 / 10%) 0px 5px 20px;
   position: relative;
-}
 `;
 
-const FrequentHeading = styled.h2`
-font-family: Roboto;
-font-style: normal;
-font-weight: 800;
-text-align: left;
-color: #333;
-`;
 
 const Question = styled.h3`
-font-family: Roboto;
-font-style: normal;
- font-size: 16px;
- color: #333;
+  font-family: Roboto;
+  font-style: normal;
+  font-size: 18px;
+  color: #333;
 `;
 
 const Content = styled.p`
-font-family: Roboto;
-font-style: normal;
- font-size: 14px;
- color: #333;
+  font-family: Roboto;
+  font-style: normal;
+  font-size: 16px;
+  color: #333;
 `;
 
 const DonKeyIconWrapper = styled.div`
-top: -40px;
-position: absolute;
-right: -14px;
-
+  top: -40px;
+  position: absolute;
+  right: -14px;
 `;
-
 
 const Ellipse1 = styled.div`
   left: 16%;
@@ -110,124 +93,111 @@ const Ellipse3 = styled.div`
 `;
 
 const Faq = () => {
-    return (
-        <>
-            <NavBar />
-            <Header>
-                <Ellipse1>
-                    <LargeEllipse />
-                </Ellipse1>
-                <Ellipse2>
-                    <LargeEllipse />
-                </Ellipse2>
-                <Ellipse3>
-                    <MeadiumEllipse />
-                </Ellipse3>
-                <div className="d-flex justify-content-center flex-wrap">
-                    <HeaderInner className="d-flex justify-content-center flex-wrap">
-                        <HeadingTitle>Don-key FAQ</HeadingTitle>
-                        <HeaderContent className="mt-5">
-                            Thanks to the invaluable feedback of the telegram community, the team is now making available a resource meant to clarify important questions surrounding the Don-key protocol, and the service that it provides to the community.
+  return (
+    <>
+      <NavBar />
+      <Header>
+        <Ellipse1>
+          <LargeEllipse />
+        </Ellipse1>
+        <Ellipse2>
+          <LargeEllipse />
+        </Ellipse2>
+        <Ellipse3>
+          <MeadiumEllipse />
+        </Ellipse3>
+        <div className="d-flex justify-content-center flex-wrap">
+          <HeaderInner className="d-flex justify-content-center flex-wrap">
+            <HeadingTitle className="mt-5">Frequently Asked Questions</HeadingTitle>
+            <HeaderContent className="mt-3">
+              Thanks to the invaluable feedback of the telegram community, the
+              team is now making available a resource meant to clarify important
+              questions surrounding the Don-key protocol, and the service that
+              it provides to the community. By the end of this week, Don-key’s
+              FAQ will also be uploaded to the home page for the community’s
+              easy access.
+            </HeaderContent>
+          </HeaderInner>
+        </div>
+      </Header>
 
-                            By the end of this week, Don-key’s FAQ will also be uploaded to the home page for the community’s easy access.
+      <Body className="d-flex justify-content-center pb-5">
+        <FrequentRoot className="p-5">
+          <DonKeyIconWrapper>
+            <DonKeyIcon />
+          </DonKeyIconWrapper>
 
-                        </HeaderContent>
-                    </HeaderInner>
+          <div className="">
+            <div className="mb-4">
+              <Question>How does the Don-key protocol work?</Question>
+              <Content>
+                Users invest their funds into farmer designed, developer created
+                yield farming strategies that are hard coded via smart
+                contracts. Liquidity within each investment pool moves exactly
+                as the smart contracts dictate.
+              </Content>
+            </div>
 
-                </div>
+            <div className="mb-4">
+              <Question>What does 1-click copy farming mean?</Question>
+              <Content>
+                Most yield farming strategies have 10 or more simultaneous and
+                recurring processes (deposit swap, harvest, claim, and so forth
+                - all on multiple protocols.) With Don-key users get to enjoy
+                the same rewards, with the unmatched ease of just a single
+                click.
+              </Content>
+            </div>
 
-            </Header>
+            <div className="mb-4">
+              <Question>Who has access to my funds?</Question>
+              <Content>
+                Absolutely no one. Your funds can’t be touched or accessed by
+                anyone but yourself. Our farmers design the strategy, and smart
+                contracts execute them.
+              </Content>
+            </div>
 
-            <Body className="d-flex justify-content-center pb-5">
+            <div className="mb-4">
+              <Question>
+                What protections are in place to secure deposited funds?
+              </Question>
+              <Content>
+                Decentralized and autonomous smart contracts keep your funds
+                secured, and completely in your control.
+              </Content>
+            </div>
 
-                <FrequentRoot className="p-5">
-                    <DonKeyIconWrapper>
-                        <DonKeyIcon />
-                    </DonKeyIconWrapper>
-                    <FrequentHeading>Frequently Asked Questions</FrequentHeading>
+            <div className="mb-4">
+              <Question>Are rewards Compounded?</Question>
+              <Content>Yes</Content>
+            </div>
 
+            <div className="mb-4">
+              <Question>How is APY per strategy calculated?</Question>
+              <Content>
+                APY stands for Annual Percentage Yield and is the projected rate
+                of annual return after taking compounding interest into account
+                - it's calculated by averaging estimated returns across a
+                strategy’s included liquidity pools.
+              </Content>
+            </div>
 
-
-
-                    <div className="mt-5">
-                        <div className="mb-4">
-                            <Question>
-                                How does the Don-key protocol work?
-                            </Question>
-                            <Content>
-                                Users invest their funds into farmer designed, developer created yield farming strategies that are hard coded via smart contracts. Liquidity within each investment pool moves exactly as the smart contracts dictate.
-                            </Content>
-                        </div>
-
-                        <div className="mb-4">
-                            <Question>
-                                What does 1-click copy farming mean?
-                            </Question>
-                            <Content>
-                                Most yield farming strategies have 10 or more simultaneous and recurring processes (deposit swap, harvest, claim, and so forth - all on multiple protocols.) With Don-key users get to enjoy the same rewards, with the unmatched ease of just a single click.
-                            </Content>
-                        </div>
-
-                        <div className="mb-4">
-                            <Question>
-                                Who has access to my funds?
-                            </Question>
-                            <Content>
-                                Absolutely no one. Your funds can’t be touched or accessed by anyone but yourself. Our farmers design the strategy, and smart contracts execute them.
-                            </Content>
-                        </div>
-
-                        <div className="mb-4">
-                            <Question>
-                                What protections are in place to secure deposited funds?
-                            </Question>
-                            <Content>
-                                Decentralized and autonomous smart contracts keep your funds secured, and completely in your control.
-                            </Content>
-                        </div>
-
-                        <div className="mb-4">
-                            <Question>
-                                Are rewards Compounded?
-                            </Question>
-                            <Content>
-                                Yes
-                            </Content>
-                        </div>
-
-
-                        <div className="mb-4">
-                            <Question>
-                                How is APY per strategy calculated?
-                            </Question>
-                            <Content>
-                                APY stands for Annual Percentage Yield and is the projected rate of annual return after taking compounding interest into account - it's calculated by averaging estimated returns across a strategy’s included liquidity pools.
-                            </Content>
-                        </div>
-
-
-                        <div >
-                            <Question>
-                                How are Don-key fees implemented? Are they percentages taken on profit or on total deposited funds?
-                            </Question>
-                            <Content>
-                                We only charge a fee on profits, not your initial investment.
-                            </Content>
-                        </div>
-
-
-
-
-
-
-                    </div>
-
-
-                </FrequentRoot>
-            </Body>
-            <Footer />
-        </>
-    )
-}
+            <div>
+              <Question>
+                How are Don-key fees implemented? Are they percentages taken on
+                profit or on total deposited funds?
+              </Question>
+              <Content>
+                We only charge a fee on profits, not your initial investment.
+              </Content>
+            </div>
+          </div>
+        </FrequentRoot>
+      </Body>
+      <Footer />
+    </>
+  );
+};
 
 export default Faq;
