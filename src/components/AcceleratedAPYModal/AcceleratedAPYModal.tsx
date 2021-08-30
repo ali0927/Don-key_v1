6881b92095320847f7b0ab633f8d7b704827127a
ali0927 <kevinali0927@gmail.com>
2021-08-30 12:38:33 +0530

    Update Accelerated APY Modal

--- src/components/AcceleratedAPYModal/AcceleratedAPYModal.tsx
+++ src/components/AcceleratedAPYModal/AcceleratedAPYModal.tsx
@@ -7,6 +7,8 @@ import { useWeb3 } from "don-components";
 import BigNumber from "bignumber.js";
 import { useStakingContract } from "hooks";
 import { Spinner } from "react-bootstrap";
+import { Slider, withStyles } from "@material-ui/core";
+import { theme } from "theme";
 
 const StyledH2 = styled.h2`
   font-weight: bold;
@@ -100,12 +102,68 @@ const DonInput = ({
   );
 };
 
-
+const DonSlider = withStyles({
+  root: {
+    color: theme.palette.common.yellow,
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  mark: {
+    width: 1,
+    height: 5,
+    marginTop: 7,
+    backgroundColor: "#b0b0b0",
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
 
 const ApyForm = styled.div`
   margin-top: 4rem;
 `;
 
+const marks = [
+  {
+    value: 20,
+    label: "Tier 1",
+  },
+  {
+    value: 40,
+    label: "Tier 2",
+  },
+  {
+    value: 60,
+    label: "Tier 3",
+  },
+  {
+    value: 80,
+    label: "Tier 4",
+  },
+  {
+    value: 100,
+    label: "Tier 5",
+  },
+];
 export const AcceleratedAPYModal = ({
   open,
   onClose,
@@ -114,8 +172,9 @@ export const AcceleratedAPYModal = ({
   onClose: () => void;
 }) => {
   const [availableDon, setAvailableDon] = useState("");
-  const [donAmount, setDonAmount] = useState("");
-  const { stakedDon,  stake, getTierInfo, coolOffDuration } = useStakingContract();
+  const [donAmount, setDonAmount] = useState("1000");
+  const { stakedDon, stake, getTierInfo, coolOffDuration } =
+    useStakingContract();
   const [predictedApy, setPredictedApy] = useState("");
   const web3 = useWeb3();
   const [loading, setLoading] = useState(false);
@@ -150,12 +209,14 @@ export const AcceleratedAPYModal = ({
     setBtnLoading(true);
     try {
       await stake(toWei(donAmount));
+      onClose();
+    } catch (e) {
+      console.log(e);
     } finally {
       setBtnLoading(false);
-      onClose()
     }
-  }
-  
+  };
+
   useEffect(() => {
     updatePredictedApy();
   }, [donAmount]);
@@ -187,31 +248,48 @@ export const AcceleratedAPYModal = ({
             placeholder="Amount"
             onChange={() => {}}
           />
-          <DonInput
-            label="Locked DON"
-            value={stakedDon}
-            placeholder="Amount"
-            onChange={() => {}}
-          />
-          <DonInput
-            label="Amount Of DON To Lock"
-            max={availableDon}
-            value={donAmount}
-            placeholder="Amount"
-            onChange={setDonAmount}
-          />
+          <span>Choose Tier</span>
+          <DonSlider
+            defaultValue={20}
+            onChange={(e, val) => {
+              const cases = {
+                "20": "1000",
+                "40": "2500",
+                "60": "5000",
+                "80": "25000",
+                "100": "50000",
+              };
+              const bnVal = new BigNumber(val as number);
+              if (bnVal.lt(20)) {
+                return setDonAmount("0");
+              }
+              if (bnVal.gt(100)) {
+                return setDonAmount(cases["100"]);
+              }
 
+              const amount = cases[val.toString() as "20"];
+              setDonAmount(amount);
+            }}
+            marks={marks}
+            step={20}
+            min={0}
+            max={110}
+          />
+          <p className="mb-1 text-center">Your DON Stake:</p>
+          <h5 className="text-center">{donAmount}</h5>
           <p className="text-center font-weight-bold px-5">
             {loading ? (
               <Spinner animation="border" size="sm" />
             ) : predictedApy !== "0" && predictedApy !== "" ? (
-              <>Extra APY Will Be: {predictedApy}%</>
+              <>Extra APY Will Be: +{predictedApy}%</>
             ) : (
               <>Minimum 1000 DON's Required To Get Extra APY</>
             )}
           </p>
 
-          <Info>The DON tokens will be locked for {coolOffDuration} after unstaking</Info>
+          <Info>
+            The DON tokens will be locked for {coolOffDuration} after unstaking
+          </Info>
           <div className="d-flex align-items-center">
             <ButtonWidget
               varaint="contained"
@@ -219,7 +297,11 @@ export const AcceleratedAPYModal = ({
               className="py-2 rounded-0 font-weight-bold"
               containedVariantColor="lightYellow"
             >
-              {btnLoading ? <Spinner animation="border" size="sm" /> : "Lock DON"}
+              {btnLoading ? (
+                <Spinner animation="border" size="sm" />
+              ) : (
+                "Lock DON"
+              )}
             </ButtonWidget>
           </div>
         </ApyForm>
