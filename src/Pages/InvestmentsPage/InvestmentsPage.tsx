4bcaba0b8ac088470ece41294c0866ae098612a8
ali0927 <kevinali0927@gmail.com>
2021-09-07 14:30:22 +0530

    UI fixes

--- src/Pages/InvestmentsPage/InvestmentsPage.tsx
+++ src/Pages/InvestmentsPage/InvestmentsPage.tsx
@@ -701,24 +701,6 @@ export const InvestmentsPage = () => {
               {renderNewInvestments()}
               {renderOldInvestments()}
               {renderNoInvestmentsFound()}
- 
-              <>
-              <Accordion>
-                      <AccordionSummary
-                          expandIcon={< div></div>}
-                          aria-controls="panel1a-content"
-                           id="panel1a-header"
-                         >
-                               <Typography>Accordion 1</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                              <Typography>
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                                 sit amet blandit leo lobortis eget.
-                             </Typography>
-                     </AccordionDetails>
-             </Accordion>
-              </>
             </Container>
           </div>
         </GridBackground>
