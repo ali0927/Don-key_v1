5a1bb5b712c69c0b774df1ae28eb41edfc2da9be
ali0927 <kevinali0927@gmail.com>
2021-09-07 11:40:20 +0530

    Fixes investment page

--- src/Pages/InvestmentsPage/StakingInfo/StakingInfo.tsx
+++ src/Pages/InvestmentsPage/StakingInfo/StakingInfo.tsx
@@ -61,9 +61,12 @@ const StakingTitle = styled.h3`
 
 const StakingSubtitle = styled.p`
   font-weight: 400;
-  font-size: 16px;
   margin-bottom: 0;
   text-align: center;
+  font-size: 14px;
+  @media only screen and (min-width: ${breakPoints.lg}) {
+    font-size: 16px;
+  }
 `;
 
 export const StyledButton = styled(ButtonWidget)`
