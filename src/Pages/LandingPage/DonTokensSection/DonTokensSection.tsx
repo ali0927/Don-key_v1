2e61a8f881dd7b691936079a6834e6cffb35c0d2
ali0927 <kevinali0927@gmail.com>
2021-09-08 19:21:16 +0530

    Landing page

--- src/Pages/LandingPage/DonTokensSection/DonTokensSection.tsx
+++ src/Pages/LandingPage/DonTokensSection/DonTokensSection.tsx
@@ -1,6 +1,32 @@
 import * as React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Heading3,LandingParagraph } from "../components";
+import Lines from "./images/img.png"
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-family: Poppins;
+  font-size: 23px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 31px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #000;
+`;
+
+const Content = styled.p`
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+ line-height: 26px;
+ letter-spacing: 0em;
+ color: ##222222;
+  text-align: left;
+  margin: 0;
+
+`;
 
 export const DonTokenSection: React.FC = () => {
   return (
@@ -20,22 +46,29 @@ export const DonTokenSection: React.FC = () => {
             <Col md={7} className="ml-md-4">
               <div>
                 <Heading3 className="mb-3 mt-md-2 mt-5">DON Tokens</Heading3>
-                <LandingParagraph>
+                <div className="d-flex">
+                <div style={{marginRight: 50, width: "18%"}}>
+                     <img style={{height: "100%"}} src={Lines} alt="Image not found"/>
+                </div>
+                <div>
+                <Content>
                   DON tokens are airdropped to all farmers based on monthly
                   trading results and take into account: ROI, Risk level, asset
                   exposure and more. the better you are , the more you get
-                </LandingParagraph>
-                <h5 className="mt-4">Yield and Dividend </h5>
-                <LandingParagraph>
+                </Content>
+                <Heading style={{marginTop:151}}>Yield and Dividend </Heading>
+                <Content>
                   DON tokens represents the farmer’s yield based on his own
                   strategies, in addition to a proportionate monthly dividend
                   from Don-key’s total monthly yield
-                </LandingParagraph>
-                <h5 className="mt-4">Exchangeable</h5>
-                <LandingParagraph>
+                </Content>
+                <Heading style={{marginTop:29}} >Exchangeable</Heading>
+                <Content>
                   You can redeem your DON tokens at any time or hold on to them
                   and accumulate higher future coupons
-                </LandingParagraph>
+                </Content>
+                </div>
+                </div>
               </div>
             </Col>
           </Row>
