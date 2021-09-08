660c048aa39498b6518aa7161957d200527bf4f2
ali0927 <kevinali0927@gmail.com>
2021-09-08 13:06:35 +0530

    Fixed Accelerated APY modal

--- src/components/AcceleratedAPYModal/AcceleratedAPYModal.tsx
+++ src/components/AcceleratedAPYModal/AcceleratedAPYModal.tsx
@@ -14,76 +14,71 @@ import clsx from "clsx";
 
 const Heading = styled.h2`
   font-weight: bold;
-  font-size: ${(props: {fontSize: string}) => props.fontSize};
+  font-size: ${(props: { fontSize: string }) => props.fontSize};
   color: #070602;
 `;
 
 const SubHeading = styled.p`
- font-family: Poppins;
- font-size: 14px;
- font-weight: 500;
- text-align: left;
- color: #A3A3A3;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: left;
+  color: #a3a3a3;
 `;
 
 const TierRoot = styled.div`
-   border: 1px solid #ECECEC;
-   border-radius: 10px;
-   padding: 5px;
-   & .tierButton {
-       width: 92px;
-       height: 37px;
-       font-family: Poppins;
-       font-size: 12px;
-       font-weight: 600;
-       color: #000;
-       display: flex;
-       align-items: center;
-       justify-content: center;
-       cursor: pointer;
-
-   }
-   & .tierSelected {
-    background: #FCEB74;
-    border: 1px solid #FED700;
+  border: 1px solid #ececec;
+  border-radius: 10px;
+  padding: 5px;
+  & .tierButton {
+    width: 92px;
+    height: 37px;
+    font-family: Poppins;
+    font-size: 12px;
+    font-weight: 600;
+    color: #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+  & .tierSelected {
+    background: #fceb74;
+    border: 1px solid #fed700;
     box-shadow: 0px 2px 10px rgba(87, 16, 112, 0.08);
     border-radius: 10px;
-    color: #081E3F;
-   }
-   & .tierDisabled {
-      color: #A2A2A2;
-   }
+    color: #081e3f;
+  }
+  & .tierDisabled {
+    color: #a2a2a2;
+  }
 `;
 
 const DonInfoRoot = styled.div`
-     margin-top: 38px;
+  margin-top: 38px;
 `;
 
 const DonAvaliableInput = styled.div`
-    height: 62px;
-    border: 1px solid #ECECEC;
-    border-radius: 10px;
-    padding: 20px;
+  height: 62px;
+  border: 1px solid #ececec;
+  border-radius: 10px;
+  padding: 20px;
 `;
 
 const HrLine = styled.hr`
-    
-     border: none;
-     &:before {
-      content: "";
-      display: block;
-      position: absolute;
-      right: 0;
-      max-width: 100%;
-      width: 100%;
-      border-top: 1px solid #ECECEC;
-    }
+  border: none;
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    right: 0;
+    max-width: 100%;
+    width: 100%;
+    border-top: 1px solid #ececec;
+  }
 `;
 
-
 const Header = styled.div``;
-
-
 
 export const AcceleratedAPYModal = ({
   open,
@@ -108,7 +103,7 @@ export const AcceleratedAPYModal = ({
   const web3 = useWeb3();
   const [loading, setLoading] = useState(false);
   const [btnLoading, setBtnLoading] = useState(false);
-  const tiersList =  getTierList();
+  const tiersList = getTierList();
   const tiersListLength = Object.keys(tiersList).length;
 
   const fetchAvailableDon = async () => {
@@ -119,31 +114,39 @@ export const AcceleratedAPYModal = ({
     setAvailableDon(toEther(userBalance));
   };
 
-  const initialTier = tier.tier ? tier.tier+1 : 1;
+  const initialTier = tier.tier ? tier.tier + 1 : 1;
   const [selectedTier, setSelectedTier] = useState(initialTier);
 
-  useEffect(() => {
+  useEffectOnTabFocus(() => {
     fetchAvailableDon();
   }, []);
 
   const donAmount = useMemo(() => {
-    let currentTier = getTierList()[selectedTier]
-    if(getTierList()[selectedTier]){
-        const amount = getTierList()[selectedTier].donRequired;
+    let currentTier = getTierList()[selectedTier];
+    if (getTierList()[selectedTier]) {
+      const amount = getTierList()[selectedTier].donRequired;
+      return amount;
+    } else {
+      currentTier = getTierList()[tier.tier];
+      if (currentTier) {
+        const amount = getTierList()[tier.tier].donRequired;
         return amount;
-    }
-    else {
-       currentTier = getTierList()[tier.tier];
-       if(currentTier){
-          const amount = getTierList()[tier.tier].donRequired;
-          return amount;
-       }
+      }
     }
     return "0";
-
   }, [selectedTier]);
 
-  
+  const requiredDons = useMemo(() => {
+    let selectedTierObj = getTierList()[selectedTier];
+
+    return new BigNumber(selectedTierObj.donRequired)
+      .minus(stakedDon)
+      .toFixed();
+  }, [selectedTier, stakedDon]);
+
+  const showBuyButton = useMemo(() => {
+    return new BigNumber(availableDon).lt(requiredDons);
+  }, [requiredDons, availableDon]);
 
   const updatePredictedApy = async () => {
     setLoading(true);
@@ -159,16 +162,18 @@ export const AcceleratedAPYModal = ({
   };
 
   const stakeDon = async () => {
-    if(initialTier < tiersListLength){
-        setBtnLoading(true);
-        try {
-           await stake(toWei(new BigNumber(donAmount).minus(stakedDon).toString()));
-           onClose();
-         } catch (e) {
-              console.log(e);
-          } finally {
-                setBtnLoading(false);
-          }
+    if (initialTier < tiersListLength) {
+      setBtnLoading(true);
+      try {
+        await stake(
+          toWei(new BigNumber(donAmount).minus(stakedDon).toString())
+        );
+        onClose();
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setBtnLoading(false);
+      }
     }
   };
 
@@ -191,10 +196,6 @@ export const AcceleratedAPYModal = ({
   }, []);
   const hasDons = hasCheckedDons && holdingDons && holdingDons.gte(100);
 
-
-
- 
-
   const renderContent = () => {
     if (!hasCheckedDons) {
       return (
@@ -209,109 +210,128 @@ export const AcceleratedAPYModal = ({
     if (hasDons) {
       return (
         <div style={{ marginTop: -30, marginBottom: -20 }}>
-          <Header >
-            <Heading fontSize="23px" className="mb-2">Accelerated APY</Heading>
+          <Header>
+            <Heading fontSize="23px" className="mb-2">
+              Accelerated APY
+            </Heading>
             <SubHeading>Stake DON tokens and get Accelerated APY</SubHeading>
           </Header>
 
           <div className="d-flex justify-content-between align-items-center mt-4">
             <SubHeading>Your current Tier:</SubHeading>
-            <Heading fontSize="27px">Tier {tier.tier }</Heading>
+            <Heading fontSize="27px">Tier {tier.tier}</Heading>
           </div>
 
           <div className="mt-2">
-             <SubHeading>Upgrade Tier</SubHeading>
+            <SubHeading>Upgrade Tier</SubHeading>
 
-             <TierRoot className="d-flex ml-0 mr-0">
-                {Object.keys(tiersList).map((item,index)=>{
-                         const tierItem = tiersList[item];
-                         const isSelected =  selectedTier === tierItem.tier;
-                         const isTierDisabled = initialTier > tierItem.tier;
-                         const isAllDisabled =  initialTier > tiersListLength;
-                         if(index !== 0){
-                         return(
-                      
-                            <div className={clsx("tierButton",{ 
-                              "tierSelected": (isSelected && !isAllDisabled),
-                              "tierDisabled": isTierDisabled,
-                            })}
-                               onClick={() => {
-                                 if(!isTierDisabled && !isAllDisabled){
-                                   setSelectedTier(tierItem.tier as number)
-                                 }
-                               }}   > Tier {tierItem.tier}</div>
-                           
-                         )
+            <TierRoot className="d-flex ml-0 mr-0">
+              {Object.keys(tiersList).map((item, index) => {
+                const tierItem = tiersList[item];
+                const isSelected = selectedTier === tierItem.tier;
+                const isTierDisabled = initialTier > tierItem.tier;
+                const isAllDisabled = initialTier > tiersListLength;
+                if (index !== 0) {
+                  return (
+                    <div
+                      className={clsx("tierButton", {
+                        tierSelected: isSelected && !isAllDisabled,
+                        tierDisabled: isTierDisabled,
+                      })}
+                      onClick={() => {
+                        if (!isTierDisabled && !isAllDisabled) {
+                          setSelectedTier(tierItem.tier as number);
                         }
-                        return null;
-                         
-                })
-
+                      }}
+                    >
+                      {" "}
+                      Tier {tierItem.tier}
+                    </div>
+                  );
                 }
-             </TierRoot>
+                return null;
+              })}
+            </TierRoot>
 
-             <DonInfoRoot>
-                 {!hasDons &&
-                     <Heading className="text-center mb-5" fontSize="20px">You don’t have DON available 
-                     in your wallet</Heading>
+            <DonInfoRoot>
+              {!hasDons && (
+                <Heading className="text-center mb-5" fontSize="20px">
+                  You don’t have DON available in your wallet
+                </Heading>
+              )}
+              {hasDons && (
+                <>
+                  <SubHeading>Available DON</SubHeading>
+                  <DonAvaliableInput>
+                    <Heading className="m-0 text-right" fontSize="25px">
+                      {new BigNumber(availableDon).toFixed(2)}
+                    </Heading>
+                  </DonAvaliableInput>
+                </>
+              )}
+            </DonInfoRoot>
 
-                 }
-                 {hasDons &&
-                 <>
-                 <SubHeading>Available DON</SubHeading>
-                 <DonAvaliableInput >
-                     <Heading className="m-0 text-right" fontSize="25px">{new BigNumber(availableDon).toFixed(2)}</Heading>
-                 </DonAvaliableInput>
-                 </>
-                 }
-
-             </DonInfoRoot>
-
-             <div className="d-flex justify-content-between align-items-center mt-3">
-                <SubHeading>Required DON Stake:</SubHeading>
-                <Heading fontSize="27px">{donAmount}</Heading>
-             </div>
-
-             <div className="d-flex align-items-center justify-content-between ">
-                <SubHeading>Extra APY will be</SubHeading>
-                <Heading fontSize="20px">+{predictedApy}%</Heading>
-             </div>
-
-             <div className="d-flex align-items-center justify-content-center mt-3">
-                 <ButtonWidget
-                    varaint="contained"
-                    onClick={stakeDon}
-                    disabled={initialTier >= tiersListLength}
-                    className="py-2 font-weight-bold"
-                    containedVariantColor="lightYellow"
-                    width="205px"
-                   height="40px"
-              >
-                   {btnLoading ? (
-                      <Spinner animation="border" size="sm" />
-                        ) : (
-                         "Lock DON"
-                    )}
-              </ButtonWidget>
+            <div className="d-flex justify-content-between align-items-center mt-3">
+              <SubHeading>Required DON Stake:</SubHeading>
+              <Heading fontSize="27px">{requiredDons}</Heading>
             </div>
 
-                <HrLine className="mt-4 mb-5"/>
+            <div className="d-flex align-items-center justify-content-between ">
+              <SubHeading>Extra APY will be</SubHeading>
+              <Heading fontSize="20px">+{predictedApy}%</Heading>
+            </div>
 
-                
-                   <SubHeading className="m-0">
-                     Staked DON tokens will be lcoked for 2 minutes after unstaking.
-                    DON rewards are claimable on the go.  <a
-                      href="https://don-key-finance.medium.com/introducing-don-key-accelerated-rewards-b27f629cb33b"
-                      target="_blank"
-                     className="ml-1"
-                     style={{color: "#000"}}
+            <div className="d-flex align-items-center justify-content-center mt-3">
+              {showBuyButton ? (
+                <ButtonWidget
+                  varaint="contained"
+                  onClick={() => {
+                    window.open(
+                      "https://pancakeswap.finance/swap?inputCurrency=0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c&outputCurrency=0x86B3F23B6e90F5bbfac59b5b2661134Ef8Ffd255",
+                      "_blank"
+                    );
+                  }}
+                  className="py-2 font-weight-bold"
+                  containedVariantColor="lightYellow"
+                  width="205px"
+                  height="40px"
+                >
+                  Buy DON
+                </ButtonWidget>
+              ) : (
+                <ButtonWidget
+                  varaint="contained"
+                  onClick={stakeDon}
+                  disabled={initialTier >= tiersListLength}
+                  className="py-2 font-weight-bold"
+                  containedVariantColor="lightYellow"
+                  width="205px"
+                  height="40px"
+                >
+                  {btnLoading ? (
+                    <Spinner animation="border" size="sm" />
+                  ) : (
+                    "Lock DON"
+                  )}
+                </ButtonWidget>
+              )}
+            </div>
+
+            <HrLine className="mt-4 mb-5" />
+
+            <SubHeading className="m-0">
+              Staked DON tokens will be locked for 2 minutes after unstaking.
+              DON rewards are claimable on the go.{" "}
+              <a
+                href="https://don-key-finance.medium.com/introducing-don-key-accelerated-rewards-b27f629cb33b"
+                target="_blank"
+                className="ml-1"
+                style={{ color: "#000" }}
               >
                 Read more{" "}
               </a>
-                    </SubHeading>
-               
+            </SubHeading>
           </div>
-
         </div>
       );
     } else {
@@ -331,7 +351,6 @@ export const AcceleratedAPYModal = ({
     </DonCommonmodal>
   );
 };
-
 
 // const DonInput = ({
 //   label,
@@ -415,8 +434,6 @@ export const AcceleratedAPYModal = ({
 //   margin-top: 4rem;
 // `;
 
-
-
 // const Info = styled.p`
 //   font-size: 12px;
 //   line-height: 20px;
@@ -469,9 +486,8 @@ export const AcceleratedAPYModal = ({
 //   }
 // `;
 
-
-
-          {/* <ApyForm>
+{
+  /* <ApyForm>
             <DonInput
               label="Available DON"
               value={new BigNumber(availableDon).toFixed(2)}
@@ -541,4 +557,5 @@ export const AcceleratedAPYModal = ({
             </div>
           
           </ApyForm>
-         */}
+         */
+}
