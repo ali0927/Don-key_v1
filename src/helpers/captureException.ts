1de8c06cfb61fa376a7855489dd804e70cd1ff77
ali0927 <kevinali0927@gmail.com>
2021-09-11 12:04:24 +0530

    Fixed Scope Passing

--- src/helpers/captureException.ts
+++ src/helpers/captureException.ts
@@ -2,5 +2,5 @@ import * as Sentry from "@sentry/react";
 export const captureException = (e: any, msg: string) => {
     const scope = new Sentry.Scope();
     scope.setExtra("Message", msg)
-    Sentry.captureException(e)
+    Sentry.captureException(e, scope);
 }
