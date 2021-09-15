60a599aec6024bca1c40a20c24c220eabc7ba65b
ali0927 <kevinali0927@gmail.com>
2021-09-15 10:16:41 +0530

    Fixes Don token section update in both src and nextjs

--- donkey-next/src/LandingPage/DonTokensSection/DonTokensSection.tsx
+++ donkey-next/src/LandingPage/DonTokensSection/DonTokensSection.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Heading3 } from "../components";
-import Lines from "./images/img.png"
+import Lines from "./images/img.png";
 import styled from "styled-components";
-import Donkey from "./images/donkey.png"
+import Donkey from "./images/donkey.png";
 import { breakPoints } from "breakponts";
 import Image from "next/image";
 const Root = styled.div`
-    margin-top: 120px;
-    margin-bottom: 120px;
+  margin-top: 120px;
+  margin-bottom: 120px;
 `;
 
 const Heading = styled.h1`
@@ -32,19 +32,17 @@ const Content = styled.p`
   color: #222222;
   text-align: left;
   margin: 0;
-
 `;
 
 const LineRoot = styled.div`
+  width: 100%;
+  @media only screen and (max-width: ${breakPoints.md}) {
+    width: 25%;
+  }
+  @media only screen and (max-width: ${breakPoints.sm}) {
     width: 100%;
-    @media only screen and (max-width: ${breakPoints.md}) {
-       width: 25%;
-    }
-    @media only screen and (max-width: ${breakPoints.sm}) {
-       width: 100%;
-    }
+  }
 `;
-
 
 export const DonTokenSection: React.FC = () => {
   return (
@@ -64,35 +62,36 @@ export const DonTokenSection: React.FC = () => {
             <Col lg={8}>
               <div>
                 <div className="d-flex">
-                <LineRoot className="d-flex justify-content-lg-end mr-5 mt-2">
-                     <Image src={Lines} alt="Image not found"/>
-                </LineRoot>
-                <div className="d-flex flex-column justify-content-between">
-                   <div>
-                      <Heading3 className="mb-3">DON Tokens</Heading3>
+                  <LineRoot className="d-flex justify-content-lg-end mr-5 mt-2">
+                    <Image src={Lines} alt="Image not found" />
+                  </LineRoot>
+                  <div className="d-flex flex-column justify-content-between">
+                    <div>
+                      <Heading3 className="mb-3">DON Token</Heading3>
                       <Content>
-                         DON tokens are airdropped to all farmers based on monthly
-                         trading results and take into account: ROI, Risk level, asset
-                         exposure and more. the better you are , the more you get
-                       </Content>
-                   </div>
-                   <div className="mt-5">
-                       <Heading >Yield and Dividend </Heading>
-                       <Content>
-                           DON tokens represents the farmer’s yield based on his own
-                           strategies, in addition to a proportionate monthly dividend
-                           from Don-key’s total monthly yield
+                        DON tokens are the full utility token for the Don-key
+                        "copy farming" platform. In order to access the DAPP and
+                        participate you need to hold at least 100 $DON in you
+                        wallet.
                       </Content>
-                  </div>
+                    </div>
+                    <div className="mt-5">
+                      <Heading>Tiers and benefits</Heading>
+                      <Content>
+                        Stake $DON and climb up the tier ladder to get more and
+                        more utility from the platform. From extra APY to access
+                        to VIP farmers, reduces fees and much more to come.
+                      </Content>
+                    </div>
 
-                  <div>
-                      <Heading  >Exchangeable</Heading>
+                    <div>
+                      <Heading>Exchangeable</Heading>
                       <Content>
-                         You can redeem your DON tokens at any time or hold on to them
-                         and accumulate higher future coupons
+                        You can redeem your $DON tokens on either Uniwsap or
+                        Pancakeswap and soon CEX listing are coming
                       </Content>
+                    </div>
                   </div>
-                </div>
                 </div>
               </div>
             </Col>
