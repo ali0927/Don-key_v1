be7bc29055973130740576b286d3c7701288adf3
ali0927 <kevinali0927@gmail.com>
2021-09-25 11:08:49 +0530

    Fixed Logo

--- donkey-gatsby/src/components/Navbar/Logo.tsx
+++ donkey-gatsby/src/components/Navbar/Logo.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import styled from "styled-components";
 import { Navigate } from "components/Navigate";
-import Logo2 from "./Logo2.png";
+import Logo2 from "./donkeylogo-black.svg";
 
 const Image = styled.img`
   position: relative;
-  width: 24px;
-  top: -2px;
-  margin-left: -2px;
-  margin-right: -1px;
+  width: 100px;
 `;
 
 export const Logo = () => {
@@ -20,7 +17,7 @@ export const Logo = () => {
         className="d-flex align-items-center inherit-color no-underline"
         to="/"
       >
-        <Image loading="eager" width="85px" height="25px" src={Logo2} alt="Don key Logo" />
+        <Image  src={Logo2} alt="Don key Logo" />
       </Navigate>
     </Navbar.Brand>
   );
