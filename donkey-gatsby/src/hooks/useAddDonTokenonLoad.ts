58128d7064c11e0e0fade4e6652bd6c06c64b0f1
ali0927 <kevinali0927@gmail.com>
2021-10-17 15:46:10 +0530

    Fixed Wrong Token Addition

--- donkey-gatsby/src/hooks/useAddDonTokenonLoad.ts
+++ donkey-gatsby/src/hooks/useAddDonTokenonLoad.ts
@@ -1,18 +1,18 @@
-import { useWeb3Context } from "don-components";
+import { BINANCE_CHAIN_ID, ETHEREUM_CHAIN_ID, useWeb3Context } from "don-components";
 import { captureException } from "helpers";
 import { useEffect } from "react";
 
 export const useAddDonTokenonLoad = () => {
-  const { chainId: network } = useWeb3Context();
+  const { chainId: network, connected } = useWeb3Context();
   useEffect(() => {
-    if (window.ethereum) {
+    if (window.ethereum && connected) {
       // Add DON token with icon on Metamask
       const tokenSymbol = "DON";
       const tokenDecimals = 18;
       const tokenImage =
         "https://don-key.fra1.digitaloceanspaces.com/farmer-icons/logo-gold.png";
       let tokenAdded = localStorage.getItem("tokenLogo");
-      if (network === 56 && !tokenAdded) {
+      if (network === BINANCE_CHAIN_ID && !tokenAdded) {
         // Binance Smart Chain
         const tokenAddress = "0x86b3f23b6e90f5bbfac59b5b2661134ef8ffd255";
         window.ethereum
@@ -38,7 +38,7 @@ export const useAddDonTokenonLoad = () => {
           .catch((err: any) => {
             captureException(err, "useAddToken on Load");
           });
-      } else if (network === 1 && !tokenAdded) {
+      } else if (network === ETHEREUM_CHAIN_ID && !tokenAdded) {
         // Ethereum Mainnet
         const tokenAddress = "0x217ddead61a42369a266f1fb754eb5d3ebadc88a";
         window.ethereum
