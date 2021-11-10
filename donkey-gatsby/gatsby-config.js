2af5f1707ef10c29bb1f871df4ff26c459cce2ad
ali0927 <kevinali0927@gmail.com>
2021-11-10 20:16:45 +0530

    Removed Plugin (#356)
    Co-authored-by: Adarshdeep Singh <adeee621@gmail.com>

--- donkey-gatsby/gatsby-config.js
+++ donkey-gatsby/gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-tsconfig-paths`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-gatsby-cloud`,
     { resolve: `gatsby-plugin-sitemap` },
     {
       resolve: `gatsby-plugin-loadable-components-ssr`,
