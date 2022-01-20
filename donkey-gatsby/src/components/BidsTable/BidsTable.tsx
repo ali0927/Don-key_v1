a22ed6bbbeec302986f0436e350e1317ef80d9bc
ali0927 <kevinali0927@gmail.com>
2022-01-20 12:02:45 -0500

    Added Claim Popup

--- donkey-gatsby/src/components/BidsTable/BidsTable.tsx
+++ donkey-gatsby/src/components/BidsTable/BidsTable.tsx
@@ -1,58 +1,80 @@
-import {TableRow} from 'components/TableRow'
+import { ClaimPopup } from "components/ClaimPopup";
+import { TableRow } from "components/TableRow";
+import { useState } from "react";
 
 export const BidsTable = () => {
-    return   <div className="sTableRowip table_sTableRowip your_bids" style={{paddingTop: '165px'}}>
-    <div className="boxed">
-      <h3>Your Bids</h3>
-      <table>
-        
-        <thead>
-          <TableRow>
-          <th>#</th>
-          <th>status</th>
-          <th>wallet</th>
-          <th>sTableRowategy lp</th>
-          <th>value</th>
-          <th>borrow</th>
-          <th>commission</th>
-          <th>action</th>
-          </TableRow>
-        </thead>
-        <tbody>
-        <TableRow>
-          <td>1</td>
-          <td data-title="status" className="status success">successful</td>
-          <td data-title="wallet">0404sjww1920223.....</td>
-          <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
-          <td data-title="value">$2,280,00</td>
-          <td data-title="borrow">$2,280,00</td>
-          <td data-title="commission">8%</td>
-          <td><button>claim</button></td>
-        </TableRow>
-        <TableRow>
-        <div className="toggle_fold">⌵</div>
-          <td>2</td>
-          <td data-title="status" className="status pending">pending</td>
-          <td data-title="wallet">qqw04sjww1920223.....</td>
-          <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
-          <td data-title="value">$2,280,00</td>
-          <td data-title="borrow">$2,280,00</td>
-          <td data-title="commission">12%</td>
-          <td><button className="white">See Details</button></td>
-        </TableRow>
-        <TableRow>
-        <div className="toggle_fold">⌵</div>
-          <td>3</td>
-          <td data-title="status" className="status rejected">rejected</td>
-          <td data-title="wallet">0608sjhw1320223.....</td>
-          <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
-          <td data-title="value">$2,280,00</td>
-          <td data-title="borrow">$2,280,00</td>
-          <td data-title="commission">10%</td>
-          <td><button className="gray">Unstake LP</button></td>
-        </TableRow>
-        </tbody>
-      </table>
+  const [openClaim, setOpenClaim] = useState(false);
+  return (
+    <div
+      className="sTableRowip table_sTableRowip your_bids"
+      style={{ paddingTop: "165px" }}
+    >
+      <div className="boxed">
+        {openClaim && (
+          <ClaimPopup open={openClaim} onClose={() => setOpenClaim(false)} />
+        )}
+        <h3>Your Bids</h3>
+        <table>
+          <thead>
+            <TableRow>
+              <th>#</th>
+              <th>status</th>
+              <th>wallet</th>
+              <th>sTableRowategy lp</th>
+              <th>value</th>
+              <th>borrow</th>
+              <th>commission</th>
+              <th>action</th>
+            </TableRow>
+          </thead>
+          <tbody>
+            <TableRow>
+              <td>1</td>
+              <td data-title="status" className="status success">
+                successful
+              </td>
+              <td data-title="wallet">0404sjww1920223.....</td>
+              <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
+              <td data-title="value">$2,280,00</td>
+              <td data-title="borrow">$2,280,00</td>
+              <td data-title="commission">8%</td>
+              <td>
+                <button onClick={() => setOpenClaim(true)}>claim</button>
+              </td>
+            </TableRow>
+            <TableRow>
+              <div className="toggle_fold">⌵</div>
+              <td>2</td>
+              <td data-title="status" className="status pending">
+                pending
+              </td>
+              <td data-title="wallet">qqw04sjww1920223.....</td>
+              <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
+              <td data-title="value">$2,280,00</td>
+              <td data-title="borrow">$2,280,00</td>
+              <td data-title="commission">12%</td>
+              <td>
+                <button className="white">See Details</button>
+              </td>
+            </TableRow>
+            <TableRow>
+              <div className="toggle_fold">⌵</div>
+              <td>3</td>
+              <td data-title="status" className="status rejected">
+                rejected
+              </td>
+              <td data-title="wallet">0608sjhw1320223.....</td>
+              <td data-title="sTableRowategy lp">DON - Curve DAL * *</td>
+              <td data-title="value">$2,280,00</td>
+              <td data-title="borrow">$2,280,00</td>
+              <td data-title="commission">10%</td>
+              <td>
+                <button className="gray">Unstake LP</button>
+              </td>
+            </TableRow>
+          </tbody>
+        </table>
       </div>
     </div>
-}
+  );
+};
---
+++ donkey-gatsby/src/components/ClaimPopup/ClaimPopup.tsx
@@ -0,0 +1,78 @@
+import { DonCommonmodal } from "components/DonModal";
+import React from "react";
+import styled, { css } from "styled-components";
+
+const StyledButton = styled.button`
+  color: #fff;
+  font-weight: 400;
+  font-size: 14px;
+  background: linear-gradient(0deg, #0e0f10 0%, #272a2f 48.04%, #131415 100%);
+  border-radius: 5px;
+  border: 0px;
+  ${(props: { variant?: "white" | "black" }) => {
+    return (
+      props.variant === "white" &&
+      css`
+        background: #fff;
+        border: 1px solid #000;
+        color: #333;
+      `
+    );
+  }}
+`;
+
+export const ClaimPopup = ({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) => {
+  return (
+    <DonCommonmodal
+      onClose={onClose}
+      variant="common"
+      title={
+        <>
+          <div
+            style={{
+              width: 52,
+              height: 52,
+              backgroundColor: "#FFF037",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <svg
+              width={25}
+              height={22}
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.563 20.313a1.56 1.56 0 0 0 1.562 1.562h7.813v-7.813H1.562v6.25Zm12.5 1.562h7.812a1.56 1.56 0 0 0 1.563-1.563v-6.25h-9.375v7.813ZM23.437 6.25h-2.055c.303-.59.493-1.245.493-1.953A4.303 4.303 0 0 0 17.578 0c-2.031 0-3.345 1.04-5.03 3.335C10.865 1.04 9.552 0 7.52 0a4.303 4.303 0 0 0-4.297 4.297c0 .708.185 1.362.493 1.953H1.562A1.56 1.56 0 0 0 0 7.813v3.906c0 .43.352.781.781.781H24.22c.43 0 .781-.352.781-.781V7.813a1.56 1.56 0 0 0-1.563-1.563Zm-15.922 0a1.953 1.953 0 1 1 0-3.906c.971 0 1.69.16 4.204 3.906H7.515Zm10.063 0h-4.204c2.51-3.735 3.208-3.906 4.204-3.906a1.953 1.953 0 1 1 0 3.906Z"
+                fill="#000"
+              />
+            </svg>
+          </div>
+        </>
+      }
+      size="sm"
+      isOpen={open}
+    >
+      <h3 className="mb-3">It’s a win!</h3>
+      <p >Congratulations. Your bid from 24/02/2021 has been won.</p>{" "}
+      <p style={{fontWeight: 100}}>You can now claim your loan. Click here to see the loan details</p>
+      <div className="d-flex my-4">
+        <div className="pr-2 w-50">
+          <StyledButton>CLAIM</StyledButton>
+        </div>
+        <div className="pl-2 w-50">
+          <StyledButton variant="white">Later</StyledButton>
+        </div>
+      </div>
+    </DonCommonmodal>
+  );
+};
