62dce5162fed460d005a69c07f4da9b5aa70b339
ali0927 <kevinali0927@gmail.com>
2022-02-08 06:17:42 -0500

    Added Logrocket

--- donkey-gatsby/src/store/index.ts
+++ donkey-gatsby/src/store/index.ts
@@ -1,11 +1,13 @@
+import LogRocket from "logrocket";
 import { applyMiddleware, compose } from "redux";
 import { createStore } from "redux";
 import thunk from "redux-thunk";
 
 import { rootReducer } from "./reducers/rootReducer";
 
+
 //@ts-ignore
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
     }) : compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk,LogRocket.reduxMiddleware())));
