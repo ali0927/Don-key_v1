526f0326cc8baf6c67009ce0b7d067ab19f48766
ali0927 <kevinali0927@gmail.com>
2022-02-10 08:58:34 -0500

    Changed URI

--- donkey-gatsby/src/store/selectors/selectAuction.ts
+++ donkey-gatsby/src/store/selectors/selectAuction.ts
@@ -4,12 +4,12 @@ import memoizeOne from "memoize-one";
 
 export const createSelectAuction = () => {
   return memoizeOne(
-    (auctions: IStoreState["auctions"]["auctionInfo"], address: string) => {
+    (auctions: IStoreState["auctions"]["auctionInfo"], auctionAddress: string) => {
       if (
         isOneOf(auctions.status, ["FETCH_BALANCE_SUCCESS", "FETCH_SUCCESS"])
       ) {
         const auctionList = (auctions as IAuctionSuccessState).auctionState;
-        return auctionList.find((item) => item.address === address);
+        return auctionList.find((item) => item.address === auctionAddress);
       }
       return null;
     }
